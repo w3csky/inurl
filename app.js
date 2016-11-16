d6b671b0836c32fae418d27f2a9e7230f674dab9
Attax <westooy@gmail.com>
2016-11-16 20:58:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,106 +35,143 @@ app.get('/favicon.ico', (req, res) => {
 });
 app.get('/addurl/', (req, res) => {
 
-            var jsonpName = req.query.callback;
-            var queryURL = req.query.url;
-            var data = {};
-            //如果添加的链接存在
-            if (queryURL !== undefined && queryURL !== '') {
-                var SQL = 'SELECT uid FROM `surl` WHERE target=' + queryURL;
+    var jsonpName = req.query.callback;
+    var queryURL = req.query.url;
+    console.log(queryURL);
+    var data = {};
+    //如果添加的链接存在
+    if (queryURL != undefined && queryURL != '') {
 
-                //查库，确认是否已经存在相应链接
-                conn.query(SQL, (err, rows, fields) => {
+        var conn = mysql.createConnection(mysqlOpt);
 
-                        if (err) {
+        var SQL = 'SELECT uid FROM `surl` WHERE target=' + queryURL;
+        console.log('unde', queryURL)
+            //查库，确认是否已经存在相应链接
+        conn.query(SQL, (err, rows, fields) => {
+
+
+            if (err) {
+                console.log()
+                console.log('mysql error:', err);
+                res.end();
+            } else {
+                //如果数据库中已经存在相应的连接
+                if (rows.length) {
+                    var uid = rows[0].uid;
+                    //如果是jsonp
+                    if (jsonpName != undefined) {
+                        res.end(jsonpName + '(' + queryURL + ')');
+                    } else {
+                        console.log(queryURL);
+                        res.end(surl.idToURL(uid));
+                    }
+                } else {
+                    //插入到数据库中
+                    var InsertSQL = 'INSERT INTO `surl` (target) VALUES (' + queryURL + ')';
+                    //获取自增id
+                    var uidSQL = 'SELECT LAST_INSERT_ID()';
+                    conn.query(InsertSQL, (err2, rows2, fields) => {
+                        if (err2) {
                             console.log()
-                            console.log('mysql error:', err);
+                            console.log('mysql error:', err2);
+                            res.end();
                         } else {
-                            //如果数据库中已经存在相应的连接
-                            if (rows.length) {
-                                var uid = rows[0].uid;
-                                res.end(surl.idToURL(uid);
-                                    //如果是jsonp
-                                    if (jsonpName != undefined) {
-                                        res.end(jsonpName + '(' + queryURL + ')');
-                                    } else {
-                                        console.log(queryURL);
-                                        res.end(queryURL);
-                                    }
-                                }
-                                else {
-                                    //插入到数据库中
-                                    var InsertSQL = 'INSERT INTO `surl` (target) VALUES (' + queryURL + ')';
-                                    conn.query(InsertSQL, (err2, rows, fields) => {
+                            conn.query(uidSQL, (err3, rows3, fields) => {
+                                if (err3) {
+                                    console.log()
+                                    console.log('mysql error:', err3);
+                                    res.end();
+                                } else {
+
+                                    if (rows3.length) {
+                                        var uid = rows3[0].uid;
                                         //如果是jsonp
                                         if (jsonpName != undefined) {
                                             res.end(jsonpName + '(' + queryURL + ')');
                                         } else {
                                             console.log(queryURL);
-                                            res.end(queryURL);
+                                            res.end(surl.idToURL(uid));
                                         }
-                                    });
+                                    }
+
                                 }
-                            }
 
-                        };
+                            });
+                        }
 
-                    } else {
-                        data.code = 404;
-                        data.result = null;
-                        console.log('链接缺失')
-                        res.end(data);
-                    }
 
-                });
-
-            //短网址跳转
-            app.get('/:surl', (req, res) => {
-                if (req.params.surl !== 'favicon.ico') {
-                    console.log('changdu:', req.params.surl)
-                    if (req.params.surl.length <= 6) {
-                        //连接mysql
-                        const conn = mysql.createConnection(mysqlOpt);
-
-                        var surlId = surl.URLToId(req.params.surl);
-
-                        var SQL = 'SELECT target FROM `surl` WHERE uId=' + surlId;
-                        //执行查询
-                        conn.query(SQL, (err, rows, fields) => {
-                            if (err) {
-                                console.log()
-                                console.log('mysql error:', err);
-                            } else {
-                                if (rows.length) {
-                                    var target = rows[0].target;
-                                    //重定向到相应链接
-                                    res.redirect(301, target);
-                                }
-                            };
-
-                            //释放mysql连接
-                            conn.end();
-                        });
-                    } else {
-                        res.status(404).send('404');
-                    }
-
+                    });
                 }
+            }
+
+
+        });
 
 
 
+    } else {
+        data.code = 404;
+        data.result = null;
+        console.log('链接缺失')
+        res.end('data.code: ' + data.code);
+    }
+
+});
+
+//短网址跳转
+app.get('/:surl', (req, res) => {
+    console.log(req.params.surl)
+
+
+    if (req.params.surl !== 'favicon.ico') {
+        console.log('changdu:', req.params.surl)
+        if (req.params.surl.length <= 6) {
+            //连接mysql
+            var conn = mysql.createConnection(mysqlOpt);
+
+            var surlId = surl.URLToId(req.params.surl);
+            console.log('surlId', surlId);
+
+            var SQL = 'SELECT target FROM `surl` WHERE uid=' + surlId;
+            //执行查询
+            conn.query(SQL, (err, rows, fields) => {
+                if (err) {
+                    console.log()
+                    console.log('mysql error:', err);
+                } else {
+                    if (rows.length) {
+                        var target = rows[0].target;
+                        //重定向到相应链接
+                        res.redirect(301, target);
+                        res.end();
+                    }
+                };
+
+                //释放mysql连接
+                conn.end();
             });
+        } else {
+            res.status(404).send('404');
+        }
+
+    }
 
 
-            //404
-            app.get('*', (req, res) => {
-                res.status(404).send('404');
-            });
 
-            app.use((err, req, res, next) => {
 
-                res.status(err.status || 500);
-                res.render('error', {
-                    message: err.message,
-                    error: {}
-                });
-            });
+});
+
+
+//404
+app.get('*', (req, res) => {
+    res.status(404).send('404');
+});
+
+app.use((err, req, res, next) => {
+
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
