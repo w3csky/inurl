d2b18e8db7c307ccc925fe087b0f53d385cf46b3
Attax <westooy@gmail.com>
2016-11-17 14:47:42 +0800

    Update core.js

--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 var dict = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,0,1,2,3,4,5,6,7,8,9';
-
-var dictTable = dict.split(',');
+//数据库id起始为1，补上0号位
+var dictTable = [''];
+dictTable = dictTable.concat(dict.split(','));
 
 var link = '';
 
